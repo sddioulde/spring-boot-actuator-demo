1837f375fa8bd67d6e7f53cd4f32c3b95dbea9b3
Saliou Diallo <sdiallo@pivotal.io>
2020-03-11 15:28:23 -0400

    Initial commit
    Generate project using Spring Initializr

---
+++ src/test/java/com/example/springbootactuatordemo/SpringBootActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootactuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
